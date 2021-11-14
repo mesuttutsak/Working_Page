3d23e0176cdb61682ebf97f7777196d53fd9d84a
mesuttutsak <mesuttutsak.icmimar@gmail.com>
2021-11-14 18:05:04 +0300

    Search was done & Icon items was fix.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const form = document.querySelector('#addForm');
-const formValue = form.value.trim();
 const addBtn = document.querySelector('#addTodo');
 const removeBtn = document.querySelector('#removeTodo');
 const filter = document.querySelector('#filter');
@@ -7,10 +6,9 @@ const todoList = document.querySelector('#todoList');
 var liItems = document.querySelector('#todoList').getElementsByTagName('li');
 // const todoListAll = document.querySelectorAll('#todoList li');
 
-
 addBtn.addEventListener('click', (e) => {
-    if (form.value !== "") {
-        newTodo(form.value)
+    if (getFormValue() !== "") {
+        newTodo(getFormValue())
         form.value = "";
     };
 });
@@ -19,7 +17,6 @@ function newTodo(text) {
     // 'li' Elementi ekleme
     const listItem = document.createElement("li");
     listItem.className = "list-group-item list-group-item-action d-flex flex-row";
-    todoList.appendChild(listItem);
 
     // 'li'nin' içine 'span' ekleme
     const listItemText = document.createElement("span");
@@ -40,42 +37,51 @@ function newTodo(text) {
     listItemIconButton1.innerHTML = "<i class='fa fa-plus-circle'></i>";
     listItemIconWrapper.appendChild(listItemIconButton1);
 
-    listItemIconButton1.addEventListener('click',(e) => {
+    listItemIconButton1.addEventListener('click', (e) => {
         newTodo(text)
-    } )
+    })
 
     //  Delete Icon
     const listItemIconButton2 = document.createElement("button");
     listItemIconButton2.className = "btn btn-default btn-xs pull-right";
-    listItemIconButton2.innerHTML = "<i class='fa fa-minus-circle bg-danger'></i>";
+    listItemIconButton2.innerHTML = "<i class='fa fa-minus-circle'></i>";
     listItemIconWrapper.appendChild(listItemIconButton2);
 
-    listItemIconButton2.addEventListener('click',(e) => {
+    listItemIconButton2.addEventListener('click', (e) => {
         deleteTodo(listItem)
-    } )    
+    })
+
+    todoList.appendChild(listItem);
 };
 
-function deleteTodo(selectItem){
+function deleteTodo(selectItem) {
     todoList.removeChild(selectItem)
 };
 
 removeBtn.addEventListener('click', (e) => {
     let listLenght = todoList.children.length;
-    for(i = 0; i < listLenght; i++) {
+    for (i = 0; i < listLenght; i++) {
         todoList.removeChild(todoList.children[0]);
     }
 });
 
-filter.addEventListener('keyup', function(ev) {
+filter.addEventListener('keyup', function (ev) {
     var text = ev.target.value;
-    var pat = new RegExp(text, 'i');
-    for (var i=0; i < liItems.length; i++) {
-      var item = liItems[i];
-      if (pat.test(item.innerText)) {
-        item.classList.remove("hidden");
-      }
-      else {(item);
-        item.classList.add("hidden");
-      }
+    var pattern = new RegExp(text, 'i');
+    for (var i = 0; i < liItems.length; i++) {
+        var item = liItems[i];
+        if (pattern.test(item.innerText)) {
+            item.classList.remove("hidden");
+            console.log("remove => \ntext =" +item.innerText + "\nclasslist = " + item.classList);
+        }
+        else {
+            item.classList.add("hidden");
+            console.log("add => \ntext =" +item.innerText + "\nclasslist = " + item.classList);
+        }
+    
     }
-  });
+});
+
+function getFormValue() {
+    return form.value.trim();
+}
